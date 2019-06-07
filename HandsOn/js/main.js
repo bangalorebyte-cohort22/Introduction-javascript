7f8002e3eff648fc4a8fc42254eaeedda889dd68
datapsyche <jithinjkumar@gmail.com>
2019-06-07 12:48:46 +0530

    Added HandsON Js

---
+++ HandsOn/js/main.js
@@ -0,0 +1,91 @@
+// console.log
+// console.log("Hello World");
+// console.warn("Jithin ");
+// console.error('This is an error');
+
+// Variable - Three ways of initializing a variable
+// let
+// var
+// const 
+
+// var name = 'jithin';
+// console.log('My name is '+ name)
+
+// console.log(`My name is ${name}`)
+
+// let name = 'jithin';
+// console.log(`My name is ${name}`);
+
+// const name_new = 'jithin j kumar';
+// console.log(`My name is ${name_new}`)
+
+// name = 'not jithin'
+// console.log(`My name is ${name}`);
+
+// name_new ='not jithin j kumar'
+// console.log(`My name is ${name_new}`)
+
+// DataTypes
+
+// const name = 'jithin';
+// const age = 28;
+// const rating = 4.5;
+// const isTrainer = true;
+// const x = null;
+// const y = undefined;
+// let z;
+// const a;
+
+// console.log(typeof z)
+// console.log(`My name is ${name} and age is ${age}`)
+
+// String Methods
+// const s = "hello world"
+// console.log(s.toUpperCase().toLowerCase())
+// console.log(s.length)
+// console.log(s.substring(0,5))
+// console.log(s.split(' '))
+
+// Arrays
+// const numbers = [1,2,3,4,5,6];
+// console.log(numbers);
+// numbers.push(7)
+// console.log(numbers);
+// numbers.unshift(0)
+// console.log(numbers);
+// numbers.pop()
+// console.log(numbers);
+
+// const fruits = ['mango','apple','oranges']
+// fruits.push('grapes')
+// console.log(fruits);
+// const newFruits = ['tomato','strawberry']
+// const something = fruits.concat(newFruits)
+// console.log(Array.isArray(something));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
